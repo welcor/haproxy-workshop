9ea1859b55d601f9e1eab464354b9a37d27ec798
Snorre Brandstadmoen <snorre.brandstadmoen@bekk.no>
2015-04-15 00:12:18 +0200

    Rename

--- docker_run_jar.sh
+++ docker_run_jar.sh
@@ -8,12 +8,13 @@ die () {
 [ "$#" -eq 3 ] || die "Eksempel: docker_run.sh 10.0.2.25 8080 apps/gs-spring-boot-0.1.0.jar"
 echo $1 | grep -E -q '^([0-9]{1,3}\.){3}[0-9]{1,3}$' || die "IP-addresse ugyldig, $1"
 echo $2 | grep -E -q '^([0-9]{1,5})$' || die "Portnummer ugyldig, $2"
-echo $3 | grep -E -q '([a-zA-Z0-9]\.jar)$' || die "Spesifiser en kjørbar .jar-fil, $3"
+echo $3 | grep -E -q '([a-zA-Z0-9]\.jar)$' || die "Spesifiser en kjÃ¸rbar .jar-fil, $3"
 
 # sudo ip addr del $1/21 dev eth0
 sudo ip addr add $1/21 dev eth0
 CONTAINER_NAME="$(echo $3 | grep -E -o '[^/]([^/]+)/?$')__$1"
-CID="$(docker run --name $CONTAINER_NAME -p $1:$2:$2 -d -v /vagrant:/opt/vagrant -it dockerfile/java:oracle-java8 java -jar /opt/vagrant/$3 )"
+echo "Running 'docker run --hostname $1 --name $CONTAINER_NAME -p $1:$2:$2 -d -v /vagrant:/opt/vagrant -it dockerfile/java:oracle-java8 java -jar /opt/vagrant/$3'"
+CID="$(docker run --hostname $1 --name $CONTAINER_NAME -p $1:$2:$2 -d -v /vagrant:/opt/vagrant -it dockerfile/java:oracle-java8 java -jar /opt/vagrant/$3 )"
 echo
 echo
 echo "Taile loggen: "
